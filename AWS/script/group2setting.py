c32916fe5e7bd87189ada3191460bb2e69b04d60
Yuehao Wang <amorno19840908@gmail.com>
2021-05-06 22:41:22 -0400

    Create group2setting.py
    final

---
+++ AWS/script/group2setting.py
@@ -0,0 +1,7 @@
+s3_bucket = 'information-arch-yuehao-wang-assignment-8a'
+rds_hostName        = "m4-test-2.cwog1e14mewr.us-east-1.rds.amazonaws.com"
+rds_userName        = "admin"
+rds_userPassword    = "12345678"
+rds_databaseName_311dw    = "311_dw"
+rds_databaseName_311    = "m4"
+rds_mysql_engine = 'mysql+pymysql://admin:12345678@m4-test-2.cwog1e14mewr.us-east-1.rds.amazonaws.com/m4'
